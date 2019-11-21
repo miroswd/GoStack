2c76de007db3af5047e6e8b4f7d1ce059a7ae8fc
miroswd <altamirsantos93@hotmail.com>
2019-11-21 20:24:33 -0300

    CRUD

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 const express = require('express'); // express exporta uma função
 
 const server = express();
-
+server.use(express.json()); // Permite a leitura de arquivo json
+                            // quando receber o dado através de body
 /* PARÂMETROS 
 
 -> Query params = ?teste=1
@@ -15,6 +16,15 @@ const server = express();
 const users = ['Diego','Cláudio','Victor','Miro','Papacu']
 
 
+
+
+// req -> busca informações
+// res -> retorna as informações
+
+
+
+
+// Lista o usuário passado pela URL, atráves de query params
 server.get('/query', (req,res) =>{
   // http://localhost:3000/query?nome=miroswd
 
@@ -22,14 +32,45 @@ server.get('/query', (req,res) =>{
   return res.json({nome:`${nome}`})
 })
 
+// Lista o usuário passado pela URL, atráves de params
 server.get('/params/:index', (req,res) => { 
   // http://localhost:3000/params/miro
   const {index} = req.params
   return res.json(users[index])
 });
 
-// req -> busca informações
-// res -> retorna as informações
+// Listando todos os usuários
+server.get('/users', (req,res) => {
+  return res.json(users)
+})
 
+
+/* Criando usuário */
+server.post('/users',(req,res) => {
+  // Preciso pegar o nome do usuário que será criado, pelo body
+  const {name} = req.body;
+  users.push(name)
+  return res.json(users)
+})
+
+
+/* Atualizando usuário */
+server.put('/users/:index',(req,res) => {
+  const {index} = req.params
+  users[index] = req.body.name
+  return res.json(users)
+})
+
+
+/* Deletando usuário */
+server.delete('/users/:index',(req,res) => {
+  const {index} = req.params;
+  users.splice(index,1) // remove uma posição, baseado no index passado
+  return res.json(users)
+})
+
+
+
+// Porta que o servidor está rodando
 server.listen(3000);
 
