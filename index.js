56b6896e55eaa1d4adbfda81052a10dd6096f930
miroswd <altamirsantos93@hotmail.com>
2019-11-21 19:18:32 -0300

    Params

--- index.js
+++ index.js
@@ -4,8 +4,25 @@ const express = require('express'); // express exporta uma função
 
 const server = express();
 
-server.get('/teste', (req,res) => {
-  return res.json({message:'in a bottle'})
+/* PARÂMETROS 
+
+-> Query params = ?teste=1
+-> Routes params = /users/:id
+-> Request body = {"name":"Miro"}
+
+*/
+
+server.get('/query', (req,res) =>{
+  // http://localhost:3000/query?nome=miroswd
+
+  const {nome} = req.query
+  return res.json({nome:`${nome}`})
+})
+
+server.get('/params/:id', (req,res) => { 
+  // http://localhost:3000/params/miro
+  const {id} = req.params
+  return res.json({message:`${id}`})
 });
 
 // req -> busca informações
